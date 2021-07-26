b42b03b7ba22bb16b26542816dce7cf223248537
achuzz777 <76623656+achuzz777@users.noreply.github.com>
2021-07-26 13:15:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer 🔰', url="https://t.me/Perfect_Vazha"
                                 )
                         ]
                     ]
